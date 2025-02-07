4b87fd03ca8145ef1595371ee8564974ea382a8b
vienlnn <vienlnn@coin98.finance>
2025-02-07 15:25:25 +0700

    feat: add fake AccessList for decode, encode RLP via transaction type

--- core/types/tx_dynamic.go
+++ core/types/tx_dynamic.go
@@ -1,3 +1,19 @@
+// Copyright 2021 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package types
 
 import (
@@ -8,16 +24,25 @@ import (
 	"github.com/tomochain/tomochain/rlp"
 )
 
+type AccessList []AccessTuple
+
+// AccessTuple is the element type of an access list.
+type AccessTuple struct {
+	Address     common.Address `json:"address"        gencodec:"required"`
+	StorageKeys []common.Hash  `json:"storageKeys"    gencodec:"required"`
+}
+
 // DynamicFeeTx represents an EIP-1559 transaction.
 type DynamicFeeTx struct {
-	ChainID   *big.Int
-	Nonce     uint64
-	GasTipCap *big.Int // a.k.a. maxPriorityFeePerGas
-	GasFeeCap *big.Int // a.k.a. maxFeePerGas
-	Gas       uint64
-	To        *common.Address `rlp:"nil"` // nil means contract creation
-	Value     *big.Int
-	Data      []byte
+	ChainID    *big.Int
+	Nonce      uint64
+	GasTipCap  *big.Int // a.k.a. maxPriorityFeePerGas
+	GasFeeCap  *big.Int // a.k.a. maxFeePerGas
+	Gas        uint64
+	To         *common.Address `rlp:"nil"` // nil means contract creation
+	Value      *big.Int
+	Data       []byte
+	AccessList AccessList
 
 	// Signature values
 	V *big.Int `json:"v" gencodec:"required"`
@@ -37,9 +62,10 @@ func (tx *DynamicFeeTx) copy() TxData {
 		ChainID:   new(big.Int),
 		GasTipCap: new(big.Int),
 		GasFeeCap: new(big.Int),
-		V:         new(big.Int),
-		R:         new(big.Int),
-		S:         new(big.Int),
+
+		V: new(big.Int),
+		R: new(big.Int),
+		S: new(big.Int),
 	}
 	if tx.Value != nil {
 		cpy.Value.Set(tx.Value)
@@ -66,16 +92,17 @@ func (tx *DynamicFeeTx) copy() TxData {
 }
 
 // accessors for innerTx.
-func (tx *DynamicFeeTx) txType() byte        { return DynamicFeeTxType }
-func (tx *DynamicFeeTx) chainID() *big.Int   { return tx.ChainID }
-func (tx *DynamicFeeTx) data() []byte        { return tx.Data }
-func (tx *DynamicFeeTx) gas() uint64         { return tx.Gas }
-func (tx *DynamicFeeTx) gasFeeCap() *big.Int { return tx.GasFeeCap }
-func (tx *DynamicFeeTx) gasTipCap() *big.Int { return tx.GasTipCap }
-func (tx *DynamicFeeTx) gasPrice() *big.Int  { return tx.GasFeeCap }
-func (tx *DynamicFeeTx) value() *big.Int     { return tx.Value }
-func (tx *DynamicFeeTx) nonce() uint64       { return tx.Nonce }
-func (tx *DynamicFeeTx) to() *common.Address { return tx.To }
+func (tx *DynamicFeeTx) txType() byte           { return DynamicFeeTxType }
+func (tx *DynamicFeeTx) chainID() *big.Int      { return tx.ChainID }
+func (tx *DynamicFeeTx) data() []byte           { return tx.Data }
+func (tx *DynamicFeeTx) gas() uint64            { return tx.Gas }
+func (tx *DynamicFeeTx) gasFeeCap() *big.Int    { return tx.GasFeeCap }
+func (tx *DynamicFeeTx) gasTipCap() *big.Int    { return tx.GasTipCap }
+func (tx *DynamicFeeTx) gasPrice() *big.Int     { return tx.GasFeeCap }
+func (tx *DynamicFeeTx) value() *big.Int        { return tx.Value }
+func (tx *DynamicFeeTx) nonce() uint64          { return tx.Nonce }
+func (tx *DynamicFeeTx) to() *common.Address    { return tx.To }
+func (tx *DynamicFeeTx) accessList() AccessList { return tx.AccessList }
 
 func (tx *DynamicFeeTx) effectiveGasPrice(dst *big.Int, baseFee *big.Int) *big.Int {
 	if baseFee == nil {
