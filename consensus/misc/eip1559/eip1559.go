1b1c6b1e55c4cffc7ea9be1ae54dc5583ba5f279
vienlnn <vienlnn@coin98.finance>
2025-03-04 17:17:16 +0700

    chore: refactor gaslimit.go

--- consensus/misc/eip1559/eip1559.go
+++ consensus/misc/eip1559/eip1559.go
@@ -6,7 +6,6 @@ import (
 	"math/big"
 
 	"github.com/tomochain/tomochain/common"
-	"github.com/tomochain/tomochain/consensus/misc"
 	"github.com/tomochain/tomochain/core/types"
 	"github.com/tomochain/tomochain/params"
 )
@@ -58,7 +57,7 @@ func VerifyEIP1559Header(config *params.ChainConfig, parent, header *types.Heade
 	if !config.IsEIP1559(parent.Number) {
 		parentGasLimit = parentGasLimit * config.ElasticityMultiplier()
 	}
-	if err := misc.VerifyGasLimit(parentGasLimit, header.GasLimit); err != nil {
+	if err := VerifyGasLimit(parentGasLimit, header.GasLimit); err != nil {
 		return err
 	}
 	if header.BaseFee == nil {
@@ -68,6 +67,23 @@ func VerifyEIP1559Header(config *params.ChainConfig, parent, header *types.Heade
 	if header.BaseFee.Cmp(expectedBaseFee) != 0 {
 		return fmt.Errorf("invalid baseFee: have %s, want %s, parentBaseFee %s, parentGasUsed %d",
 			header.BaseFee, expectedBaseFee, parent.BaseFee, parent.GasUsed)
+	}
+	return nil
+}
+
+// VerifyGaslimit verifies the header gas limit according increase/decrease
+// in relation to the parent gas limit.
+func VerifyGasLimit(parentGasLimit, headerGasLimit uint64) error {
+	diff := int64(parentGasLimit) - int64(headerGasLimit)
+	if diff < 0 {
+		diff *= -1
+	}
+	limit := parentGasLimit / params.GasLimitBoundDivisor
+	if uint64(diff) >= limit {
+		return fmt.Errorf("invalid gas limit: have %d, want %d +-= %d", headerGasLimit, parentGasLimit, limit-1)
+	}
+	if headerGasLimit < params.MinGasLimit {
+		return fmt.Errorf("invalid gas limit below %d", params.MinGasLimit)
 	}
 	return nil
 }
